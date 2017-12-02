aa790a1faa84ae9be919b298102bdb1fe3599d5c
Kotaro Mochizuki <mochizuki@basicinc.jp>
2017-12-02 19:35:29 +0900

    Add three-orbit-controls

---
+++ download_three_js.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+cd public/css
+curl -L -O http://threejs.org/build/three.min.js
+curl -L -O https://raw.githubusercontent.com/dataarts/dat.gui/master/build/dat.gui.min.js
+curl -L -O https://raw.githubusercontent.com/mrdoob/stats.js/master/build/stats.min.js
--- src/main.js
+++ src/main.js
@@ -1,4 +1,55 @@
-console.log('Start main');
+var scene
+var camera
 
-import sample from './sample'
-sample()
+var w = window.innerWidth
+var h = window.innerHeight
+
+
+function init () {
+
+  // scene
+  scene = new THREE.Scene()
+
+  // camera
+  camera = new THREE.PerspectiveCamera(
+    45, w / h, 1, 1000
+  )
+  camera.position.set(-10, 10, 20)
+  camera.lookAt(scene.position)
+  scene.add(camera)
+
+  // orbit controls
+  var OrbitControls = require('three-orbit-controls')(THREE)
+  var controls = new OrbitControls(camera)
+
+  // renderer
+  var renderer = new THREE.WebGLRenderer()
+  renderer.setClearColor(new THREE.Color(0xdddddd))
+  renderer.setSize(w, h)
+
+  // axes
+  var axes = new THREE.AxesHelper(20)
+  scene.add(axes)
+
+  // spot light
+  var spotLight = new THREE.SpotLight(0xffffff)
+  spotLight.position.set(-10, 15, 5)
+  scene.add(spotLight)
+
+  // cube
+  var cubeGeometry = new THREE.BoxGeometry(4, 4, 4)
+  var cubeMaterial = new THREE.MeshLambertMaterial({ color: 0xff00aa })
+  var cube = new THREE.Mesh(cubeGeometry, cubeMaterial)
+  scene.add(cube)
+
+  document.getElementById('webgl').appendChild(renderer.domElement)
+
+  function animate() {
+      requestAnimationFrame(animate);
+      renderer.render(scene, camera);
+  }
+  animate();
+}
+
+
+window.onload = init
